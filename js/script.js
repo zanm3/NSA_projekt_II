32605848f3de2aaf29f0833f6b7ab3d47028aa7d
zanm3 <zan.markuza@tscng.org>
2024-12-22 18:54:41 +0000

    update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function info(){
+    Swal.fire({
+        title: 'O strani',
+        html: 'Razvijalec: Žan Markuža<br>Razred: 4.RB<br>Mentor: dr. Boštjan Vouk',
+        icon: 'info',
+        confirmButtonText: 'Ok'
+      });
+}
+window.info = info;
