9fb4dc8b148f86693e5f264d8857176fe407b718
zanm3 <zan.markuza@tscng.org>
2025-01-03 11:46:31 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function info(){
     Swal.fire({
         title: 'O strani',
-        html: 'Razvijalec: Žan Markuža<br>Razred: 4.RB<br>Mentor: dr. Boštjan Vouk',
+        html: 'Razvijalec: Žan Markuža<br>Razred: 4. RB<br>Mentor: dr. Boštjan Vouk<br>Predloga: Little Fashion (Toolplate)',
         icon: 'info',
         confirmButtonText: 'Ok'
       });
